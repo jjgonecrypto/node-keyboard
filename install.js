1ff3387af1325808a0a2e61aa0d028cd26a3f6ef
justin j. moses <justinjmoses@gmail.com>
2016-05-13 10:50:15 -0400

    Basic instrumentation

--- install.js
+++ install.js
@@ -1,6 +1,7 @@
-'use strict'
+'use strict';
 
-const path = require('path');
-
+const path = require('path')
 const clone = require('git-clone')
-clone('git@github.com:gleitz/midi-js-soundfonts.git', path.join(process.cwd(), 'node_modules', 'midi-js-soundfonts'));
+
+// download soundfonts (not on npm =(
+clone('git@github.com:gleitz/midi-js-soundfonts.git', path.join(process.cwd(), 'midi-js-soundfonts'))
