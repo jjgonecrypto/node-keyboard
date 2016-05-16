dcecc808e14b084c8be90d665e9d4c75255062b9
justin j. moses <justinjmoses@gmail.com>
2016-05-16 18:09:37 -0400

    Cleaning up install phase

--- install.js
+++ install.js
@@ -3,5 +3,12 @@
 const path = require('path')
 const clone = require('git-clone')
 
-// download soundfonts (not on npm =(
-clone('git@github.com:gleitz/midi-js-soundfonts.git', path.join(process.cwd(), 'midi-js-soundfonts'))
+const soundfontsCloneSource = 'git@github.com:gleitz/midi-js-soundfonts.git'
+
+// download soundfonts (because they aren't on npm) =(
+console.log('Downloading soundfonts...')
+console.log(`(cloning ${soundfontsCloneSource} to ${path.join(process.cwd(), 'midi-js-soundfonts')})`)
+
+clone(soundfontsCloneSource, path.join(process.cwd(), 'midi-js-soundfonts'), () => {
+    console.log('Download complete.')
+})
